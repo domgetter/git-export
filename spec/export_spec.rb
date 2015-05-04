ad6366a3ce414ba13ef3df8d9a0b310177465478
Dominic Muller <nicklink483@gmail.com>
2015-05-04 10:20:29 -0700

    added spec.  can now recognize multiple key-value pairs

--- spec/export_spec.rb
+++ spec/export_spec.rb
@@ -5,4 +5,10 @@ describe "git-export" do
     json = Git::Export.export(file)
     expect(json).to eq "{\n  \"global\": {\n    \"user.name\": \"John Smith\"\n  }\n}"
   end
+  
+  it "supports simple section with several key-value pairs" do
+    file = "[user]\n\tname = John Smith\n\temail = jsmith@gg.com"
+    json = Git::Export.export(file)
+    expect(json).to eq "{\n  \"global\": {\n    \"user.name\": \"John Smith\",\n    \"user.email\": \"jsmith@gg.com\"\n  }\n}"
+  end
 end
