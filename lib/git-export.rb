53e5b343db9a31b50c3ead7f25af7f312399259c
Dominic Muller <nicklink483@gmail.com>
2015-05-04 10:15:46 -0700

    can now recognize a section with a key-value pair

--- lib/git-export.rb
+++ lib/git-export.rb
@@ -4,30 +4,40 @@ require 'json'
 module Git
   class Export
     def self.export(file_contents)
+      
+      config_settings = {}
+      current_section = ""
+      current_key = ""
+      current_value = ""
+      
       file_contents.each_line do |line|
         if section?(line)
           current_section = section_name_from(line)
         elsif key_value_pair?(line)
           current_key, current_value = key_and_value_from(line)
+          config_settings[current_section + "." + current_key] = current_value
         end
-        
       end
+      
+      config_settings = {"global"=>config_settings}
+      JSON.pretty_generate(config_settings)
     end
     
     def self.section?(line)
-    
+      line.strip.match(/^\[([^\]]+)\]$/)
     end
     
     def self.section_name_from(line)
-    
+      line.strip.match(/^\[([^\]]+)\]$/)[1]
     end
     
     def self.key_value_pair?(line)
-    
+      line.strip.match(/^[^=]+ = [^=]+$/)
     end
     
     def self.key_and_value_from(line)
-    
+      m = line.strip.match(/^([^=]+) = ([^=]+)$/)
+      return m[1], m[2]
     end
   end
 end
