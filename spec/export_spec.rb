aa0543d96cb941b626a01a419e164c99dd1e53ed
Dominic Muller <nicklink483@gmail.com>
2015-05-04 10:25:17 -0700

    recognizes multiple simple sections

--- spec/export_spec.rb
+++ spec/export_spec.rb
@@ -11,4 +11,10 @@ describe "git-export" do
     json = Git::Export.export(file)
     expect(json).to eq "{\n  \"global\": {\n    \"user.name\": \"John Smith\",\n    \"user.email\": \"jsmith@gg.com\"\n  }\n}"
   end
+  
+  it "supports more than one section, each with one key-value pair" do
+    file = "[user]\n\tname = John Smith\n[alias]\n\tc = commit -m"
+    json = Git::Export.export(file)
+    expect(json).to eq "{\n  \"global\": {\n    \"user.name\": \"John Smith\",\n    \"alias.c\": \"commit -m\"\n  }\n}"
+  end
 end
